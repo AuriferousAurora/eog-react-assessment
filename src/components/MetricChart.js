e94e8fc63cd6d86826b5b8fff282bc465cd96447
Kyle Everett Lang <kyle.everett.lang@gmail.com>
2019-10-16 17:26:57 -0500

    Element type is invalid error.

--- src/components/MetricChart.js
+++ src/components/MetricChart.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { ResponsiveContainer } from "recharts";
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from "recharts";
 
 export default () => {
-  const data = [{metric: 'Tubing Pressure', value: 12}]
+  const [data, setData] = useState();
+
   return (
     <ResponsiveContainer width={700} height="80%">
       <LineChart width={730} height={250} data={data}
@@ -16,6 +17,7 @@ export default () => {
         <Line type="monotone" dataKey="pv" stroke="#8884d8" />
         <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
       </LineChart>
+      <button onClick={() => setData(data['value'] + 1)}>Click to increase value.</button>
     </ResponsiveContainer>
   );
 };
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,10 +6,13 @@ import { useGeolocation } from "react-use";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Chip from "./Chip";
 
+
+// I'm guessing that this function returns an object that allows you to interact with the GraphQL API.
 const client = createClient({
   url: "https://react.eogresources.com/graphql"
 });
 
+// Establish a vairable that contains the query you want to send.
 const query = `
 query($latLong: WeatherQuery!) {
   getWeatherForLocation(latLong: $latLong) {
@@ -20,6 +23,9 @@ query($latLong: WeatherQuery!) {
 }
 `;
 
+// Function getWeather, takes on argument (state) and grabs the
+// temperatureFahrenheit, description, and locationName from the state object.
+// It then returns those in what looks to be either object literal syntax or GraphQL query syntax.
 const getWeather = state => {
   const { temperatureinFahrenheit, description, locationName } = state.weather;
   return {
@@ -29,6 +35,7 @@ const getWeather = state => {
   };
 };
 
+// This is the default function which is a component. It's the weather component wrapped in a Provider.
 export default () => {
   return (
     <Provider value={client}>
@@ -37,25 +44,34 @@ export default () => {
   );
 };
 
+// Here's that weather component.
 const Weather = () => {
+  // This instantiates the GeoLocation thing from react-use.
   const getLocation = useGeolocation();
-  // Default to houston
+  // This variable gets either the input lat and long or defaults to Houston.
+  // There doesn't seem to be a way to change the location, so it probably only gets Houston.
   const latLong = {
     latitude: getLocation.latitude || 29.7604,
     longitude: getLocation.longitude || -95.3698
   };
+  // I'll need to look into this.
   const dispatch = useDispatch();
+  // So, getWeather seems to be a special kind of function that gets passed to useSelector.
+  // I'll have to look into that. I've got some documentation to read.
   const { temperatureinFahrenheit, description, locationName } = useSelector(
     getWeather
   );
-
+  
+  // An array of results is returned from this code. I'll really have to read these docs.
   const [result] = useQuery({
     query,
     variables: {
       latLong
     }
   });
+  // Grab out fetching, data, and error from the result array (at least I think it's an array.)
   const { fetching, data, error } = result;
+  // Definitely going to need to reread the hook documentaion.
   useEffect(
     () => {
       if (error) {
