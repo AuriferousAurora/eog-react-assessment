03e6e98962b4b2a040bd2808cf043967875dbe85
Kyle Everett Lang <kyle.everett.lang@gmail.com>
2019-10-28 08:18:13 -0500

    Refactored convertMilliseconds and chartData conversion logic.

--- src/components/MetricHistory.js
+++ src/components/MetricHistory.js
@@ -3,37 +3,31 @@ import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from "recharts"
 
 export default (props) => {
 
-  const convertMilliseconds = (stateObject) => {
-    for (let i = 0; i < stateObject.length; i += 1) {
-      const d = new Date(stateObject[i].at);
-      let hours = d.getHours();
-      if (hours > 12) hours = hours - 12;
-      const minutes = d.getMinutes();
-      stateObject[i].at = `${hours}:${minutes}`;
+  const metrics = Object.keys(props.allMetrics);
+  const objLength = metrics.length;
+  const arrayLength = props.allMetrics[metrics[0]].length;
+
+  const convertMilliseconds = (allMetrics) => {
+    for (let i = 0; i < objLength; i += 1) {
+      for (let j = 0; j < arrayLength; j += 1) {
+        const date = new Date(allMetrics[metrics[i]][j].at);
+        let hours = date.getHours();
+        if (hours > 12) hours = hours - 12;
+        const minutes = date.getMinutes();
+        allMetrics[metrics[i]][j].at = `${hours}:${minutes}`;
+      }
     }
   }
 
-  if (typeof(props.allMetrics.tubingPressure[0].at) === "number") {
-    convertMilliseconds(props.allMetrics.tubingPressure);
-    convertMilliseconds(props.allMetrics.flareTemp);
-    convertMilliseconds(props.allMetrics.injValveOpen);
-    convertMilliseconds(props.allMetrics.oilTemp);
-    convertMilliseconds(props.allMetrics.casingPressure);
-    convertMilliseconds(props.allMetrics.waterTemp);
-  }
-
-
-  const dataKeyArray = Object.keys(props.allMetrics);
-  const arrayLength = props.allMetrics[dataKeyArray[0]].length;
-  const objLength = dataKeyArray.length;
+  if (typeof(props.allMetrics.tubingPressure[0].at) === "number") convertMilliseconds(props.allMetrics);
 
   let chartData = [];
 
   for (let i = 0; i < arrayLength; i += 1) {
     let chartItem = {};
     for (let j = 0; j < objLength; j += 1) {
-      if (j === 0) chartItem["time"] = props.allMetrics[dataKeyArray[j]][i].at;
-      chartItem[dataKeyArray[j] + "Value"] = props.allMetrics[dataKeyArray[j]][i].value 
+      if (j === 0) chartItem["time"] = props.allMetrics[metrics[j]][i].at;
+      chartItem[metrics[j] + "Value"] = props.allMetrics[metrics[j]][i].value 
     }
     chartData.push(chartItem);
   }
