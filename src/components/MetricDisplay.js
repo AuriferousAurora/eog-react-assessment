a1ea85ba4f3c93935c9464613743ba20602f0383
Kyle Everett Lang <kyle.everett.lang@gmail.com>
2019-10-27 20:52:48 -0500

    Removing metric functionality added.

--- src/components/MetricDisplay.js
+++ src/components/MetricDisplay.js
@@ -7,12 +7,18 @@ import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
 import LinearProgress from "@material-ui/core/LinearProgress";
 
 const useStyles = makeStyles({
+  metricWrapper: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+    width: "100%",
+  },
   metricHeader: {
     display: "flex",
     height: "20%",
     width: "100%",
   },
-  metricHeader__cards: {
+  metricHeader__cardContainer: {
     height: "100%",
     width: "70%",
     background: "blue",
@@ -28,8 +34,12 @@ const useStyles = makeStyles({
   metricHeader__inputSelection: {
     width: "50%",
   },
-  hidden: {
-    display: "hidden",
+  metricBody__chartContainer: {
+    alignItems: "center",
+    display: "flex",
+    height: "100%",
+    justifyContent: "center",
+
   }
 });
 
@@ -111,6 +121,10 @@ const MetricDisplay = () => {
     }
   };
 
+  const removeMetric = metricName => {
+    setActiveMetrics(activeMetrics.filter(metric => metric !== metricName));
+  }
+
   const metricInputs = [
     {
       value: 'tubingPressure',
@@ -174,13 +188,16 @@ const MetricDisplay = () => {
     }
     chartData.push(chartItem);
   }
-  console.log(chartData);
+
+  const cards = activeMetrics.map((metricName) => 
+    <div key={metricName} id={metricName} onClick={() => removeMetric(metricName)}>{metricName}</div>
+  );
 
   return (
     <div className={classes.metricWrapper}>
       <div className={classes.metricHeader}>
-        <div className={classes.metricHeader__cards}>
-          {activeMetrics}
+        <div className={classes.metricHeader__cardContainer}>
+          {cards}
         </div>
         <div className={classes.metricHeader__inputSelectionContainer}>
 
@@ -198,37 +215,41 @@ const MetricDisplay = () => {
                 {option.label}
               </MenuItem>
             ))}
-        </TextField>
+          </TextField>
 
         </div>
       </div>
-      
-      
-      <LineChart width={1200} height={400} data={chartData}
-        margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="time" interval={200}/>
 
-        <YAxis yAxisId="tubingPressure" hide={activeMetrics.includes("tubingPressure") ? false : true} />
-        <YAxis yAxisId="flareTemp" hide={activeMetrics.includes("flareTemp") ? false : true} />
-        <YAxis yAxisId="injValveOpen" hide={activeMetrics.includes("injValveOpen") ? false : true} />
-        <YAxis yAxisId="oilTemp" hide={activeMetrics.includes("oilTemp") ? false : true} />
-        <YAxis yAxisId="casingPressure" hide={activeMetrics.includes("casingPressure") ? false : true} />
-        <YAxis yAxisId="waterTemp" hide={activeMetrics.includes("waterTemp") ? false : true} />
+      <div className={classes.metricBody__chartContainer}>
+      
+        <LineChart width={1200} height={400} data={chartData}
+          margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="time" interval={200}/>
 
-        {activeMetrics.includes("tubingPressure") ?
-        <Line yAxisId="tubingPressure" type="monotone" dataKey="tubingPressureValue" stroke="#8884d8" dot={false} /> : ''}
-        {activeMetrics.includes("flareTemp") ?
-        <Line yAxisId="flareTemp" type="monotone" dataKey="flareTempValue" stroke="#c33c54" dot={false} /> : ''}
-        {activeMetrics.includes("injValveOpen") ?
-        <Line yAxisId="injValveOpen" type="monotone" dataKey="injValveOpenValue" stroke="#7bc950" dot={false} /> : ''}
-        {activeMetrics.includes("oilTemp") ?
-        <Line yAxisId="oilTemp" type="monotone" dataKey="oilTempValue" stroke="#ddf0ff" dot={false} /> : ''}
-        {activeMetrics.includes("casingPressure") ?
-        <Line yAxisId="casingPressure" type="monotone" dataKey="casingPressureValue" stroke="#e9ce2c" dot={false} /> : ''}
-        {activeMetrics.includes("waterTemp") ?
-        <Line yAxisId="waterTemp" type="monotone" dataKey="waterTempValue" stroke="#03f7eb" dot={false} /> : ''}
-      </LineChart>
+          <YAxis yAxisId="tubingPressure" hide={activeMetrics.includes("tubingPressure") ? false : true} />
+          <YAxis yAxisId="flareTemp" hide={activeMetrics.includes("flareTemp") ? false : true} />
+          <YAxis yAxisId="injValveOpen" hide={activeMetrics.includes("injValveOpen") ? false : true} />
+          <YAxis yAxisId="oilTemp" hide={activeMetrics.includes("oilTemp") ? false : true} />
+          <YAxis yAxisId="casingPressure" hide={activeMetrics.includes("casingPressure") ? false : true} />
+          <YAxis yAxisId="waterTemp" hide={activeMetrics.includes("waterTemp") ? false : true} />
+
+          {activeMetrics.includes("tubingPressure") ?
+          <Line yAxisId="tubingPressure" type="monotone" dataKey="tubingPressureValue" stroke="#8884d8" dot={false} /> : ''}
+          {activeMetrics.includes("flareTemp") ?
+          <Line yAxisId="flareTemp" type="monotone" dataKey="flareTempValue" stroke="#c33c54" dot={false} /> : ''}
+          {activeMetrics.includes("injValveOpen") ?
+          <Line yAxisId="injValveOpen" type="monotone" dataKey="injValveOpenValue" stroke="#7bc950" dot={false} /> : ''}
+          {activeMetrics.includes("oilTemp") ?
+          <Line yAxisId="oilTemp" type="monotone" dataKey="oilTempValue" stroke="#ddf0ff" dot={false} /> : ''}
+          {activeMetrics.includes("casingPressure") ?
+          <Line yAxisId="casingPressure" type="monotone" dataKey="casingPressureValue" stroke="#e9ce2c" dot={false} /> : ''}
+          {activeMetrics.includes("waterTemp") ?
+          <Line yAxisId="waterTemp" type="monotone" dataKey="waterTempValue" stroke="#03f7eb" dot={false} /> : ''}
+        </LineChart>
+
+      </div>
+
     </div>
   );
   }
