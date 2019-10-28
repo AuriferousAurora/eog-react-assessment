386c05ea64faaf2eb1866090fd0b5a207765db1e
Kyle Everett Lang <kyle.everett.lang@gmail.com>
2019-10-28 02:40:04 -0500

    Discovered cause of pulsing animation was due to frequent rerenders. Current solution is to set isAnimationActive to false for each Line component in Linechart.

--- src/components/MetricDisplay.js
+++ src/components/MetricDisplay.js
@@ -295,8 +295,7 @@ const MetricDisplay = () => {
       
         <LineChart 
           width={1200} height={400} data={chartData}
-          margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
-          isAnimationActive={false}>
+          margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="time" interval={200}/>
 
@@ -308,17 +307,29 @@ const MetricDisplay = () => {
           <YAxis yAxisId="waterTemp" hide={activeMetrics.includes("waterTemp") ? false : true} />
 
           {activeMetrics.includes("tubingPressure") ?
-          <Line yAxisId="tubingPressure" type="monotone" dataKey="tubingPressureValue" stroke="#8884d8" dot={false} /> : ''}
+          <Line 
+            yAxisId="tubingPressure" type="monotone" dataKey="tubingPressureValue" 
+            stroke="#8884d8" dot={false} isAnimationActive={false}/> : ''}
           {activeMetrics.includes("flareTemp") ?
-          <Line yAxisId="flareTemp" type="monotone" dataKey="flareTempValue" stroke="#c33c54" dot={false} /> : ''}
+          <Line 
+            yAxisId="flareTemp" type="monotone" dataKey="flareTempValue" 
+            stroke="#c33c54" dot={false} isAnimationActive={false} /> : ''}
           {activeMetrics.includes("injValveOpen") ?
-          <Line yAxisId="injValveOpen" type="monotone" dataKey="injValveOpenValue" stroke="#7bc950" dot={false} /> : ''}
+          <Line 
+            yAxisId="injValveOpen" type="monotone" dataKey="injValveOpenValue" 
+            stroke="#7bc950" dot={false} isAnimationActive={false} /> : ''}
           {activeMetrics.includes("oilTemp") ?
-          <Line yAxisId="oilTemp" type="monotone" dataKey="oilTempValue" stroke="#e11584" dot={false} /> : ''}
+          <Line 
+            yAxisId="oilTemp" type="monotone" dataKey="oilTempValue" 
+            stroke="#e11584" dot={false} isAnimationActive={false} /> : ''}
           {activeMetrics.includes("casingPressure") ?
-          <Line yAxisId="casingPressure" type="monotone" dataKey="casingPressureValue" stroke="#e9ce2c" dot={false} /> : ''}
+          <Line 
+            yAxisId="casingPressure" type="monotone" dataKey="casingPressureValue" 
+            stroke="#e9ce2c" dot={false} isAnimationActive={false} /> : ''}
           {activeMetrics.includes("waterTemp") ?
-          <Line yAxisId="waterTemp" type="monotone" dataKey="waterTempValue" stroke="#03f7eb" dot={false} /> : ''}
+          <Line 
+            yAxisId="waterTemp" type="monotone" dataKey="waterTempValue" 
+            stroke="#03f7eb" dot={false} isAnimationActive={false} /> : ''}
 
           <Tooltip />
         </LineChart>
